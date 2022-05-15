ca9d1aa69f27d0f7c44fb8f9bec5a520f048855c
YalandHong <yalandhong@foxmail.com>
2022-05-15 20:13:31 +0800

    reformat code, delete useless things

--- wx_monitor.py
+++ wx_monitor.py
@@ -11,7 +11,6 @@ import hashlib
 import sys
 
 from pyquery import PyQuery
-from sklearn.utils import deprecated
 
 from api import get_history_api, get_html_api, get_article_comments_api, split_article_url2mis, \
     get_article_read_like_api
@@ -24,13 +23,15 @@ from settings import SLEEP_TIME, WX_REDIS_CONFIG, WX_CHAT_WND_NAME, WX_UPDATE_TI
 
 
 def delete_key_uin(account_biz):
-    redis_server = redis.StrictRedis(connection_pool=redis.ConnectionPool(**WX_REDIS_CONFIG))
+    redis_server = redis.StrictRedis(connection_pool=redis.ConnectionPool(
+        **WX_REDIS_CONFIG))
     hash_key = hashlib.md5(account_biz.encode("utf-8")).hexdigest()
     redis_server.delete(hash_key)
 
 
 def _get_key_uin(account_biz):
-    redis_server = redis.StrictRedis(connection_pool=redis.ConnectionPool(**WX_REDIS_CONFIG))
+    redis_server = redis.StrictRedis(connection_pool=redis.ConnectionPool(
+        **WX_REDIS_CONFIG))
     hash_key = hashlib.md5(account_biz.encode("utf-8")).hexdigest()
     return redis_server.get(hash_key)
 
@@ -49,12 +50,13 @@ def get_key_uin(account_biz):
 def check_key_uin(account_biz):
     try:
         key_uin_dict = get_key_uin(account_biz)
-        get_history_api(**{
-            "key": key_uin_dict.get("key", ""),
-            "uin": key_uin_dict.get("uin", ""),
-            "biz": account_biz,
-            "offset": 0,
-        })
+        get_history_api(
+            **{
+                "key": key_uin_dict.get("key", ""),
+                "uin": key_uin_dict.get("uin", ""),
+                "biz": account_biz,
+                "offset": 0,
+            })
     except KeyExpireError:
         delete_key_uin(account_biz)
         raise NoneKeyUinError(f"key: 已过期")
@@ -71,7 +73,7 @@ def get_pass_key_and_uin(article_url: str, account_biz: str):
             print(e.args)
             time.sleep(0.2)
         finally:
-            #time.sleep(2)    
+            #time.sleep(2)
             wx_chat.close_web()  # 暂时不确定 关闭窗口 或者不关闭 的影响
             time.sleep(2)
             key_uin = _get_key_uin(account_biz)
@@ -90,22 +92,16 @@ class _MonitorThread(threading.Thread):
 
     @staticmethod
     def articles(**filter_by):
-        article_list = models.Article.query.filter_by(
-            **filter_by
-        ).order_by(
-            models.Article.article_publish_time.desc()
-        ).all()
+        article_list = models.Article.query.filter_by(**filter_by).order_by(
+            models.Article.article_publish_time.desc()).all()
         db.session.commit()
         print("Article Len:", len(article_list))
         return article_list
 
     @staticmethod
     def accounts(**filter_by):
-        account_list = models.Account.query.filter_by(
-            **filter_by
-        ).order_by(
-            models.Account.created.desc()
-        ).all()
+        account_list = models.Account.query.filter_by(**filter_by).order_by(
+            models.Account.created.desc()).all()
         db.session.commit()
         return account_list
 
@@ -122,14 +118,6 @@ class _MonitorThread(threading.Thread):
             self.start_run()
         except Exception as e:
             print(repr(e))
-        # while 1:
-        #     try:
-        #         self.start_run()
-        #     except Exception as e:
-        #         print(e.args)
-        #         if MONITOR_ERROR:
-        #             raise
-        #     time.sleep(0.1)
 
     def start_run(self):
         pass
@@ -146,7 +134,9 @@ class History(_MonitorThread):
     def load_accounts(self, **filter_by):
         account_list = self.accounts(**filter_by)
         print("full account list:", account_list)
-        return [account for account in account_list if account.status in [1, 2]]
+        return [
+            account for account in account_list if account.status in [1, 2]
+        ]
 
     @staticmethod
     def save_article(account_id, article_item):
@@ -156,10 +146,7 @@ class History(_MonitorThread):
         ).count()
         new_article = False
         if counts == 0:
-            article = models.Article(
-                **article_item,
-                account_id=account_id
-            )
+            article = models.Article(**article_item, account_id=account_id)
             print(article)
             db.session.add(article)
             db.session.commit()
@@ -179,15 +166,18 @@ class History(_MonitorThread):
                 break
             s_time = time.time()
             try:
-                histories = get_history_api(**{
-                    "key": key_uin_dict.get("key", ""),
-                    "uin": key_uin_dict.get("uin", ""),
-                    "biz": account_biz,
-                    "offset": offset,
-                })
+                histories = get_history_api(
+                    **{
+                        "key": key_uin_dict.get("key", ""),
+                        "uin": key_uin_dict.get("uin", ""),
+                        "biz": account_biz,
+                        "offset": offset,
+                    })
                 ending = histories['ending']
                 next_offset = histories["next_offset"]
-                print(f"biz: {account_biz} offset: {offset} next_offset: {next_offset}")
+                print(
+                    f"biz: {account_biz} offset: {offset} next_offset: {next_offset}"
+                )
                 article_items = histories["results"]["article_infos"]
                 new_article = False
                 for article_item in article_items:
@@ -196,9 +186,11 @@ class History(_MonitorThread):
                     if new_article:
                         self.save_article(account_id, article_item)
                     else:
-                        new_article = self.save_article(account_id, article_item)
+                        new_article = self.save_article(
+                            account_id, article_item)
 
-                account.counts = models.Article.query.filter_by(account_id=account.id).count()
+                account.counts = models.Article.query.filter_by(
+                    account_id=account.id).count()
                 if account_offset == 0:
                     account.offset = offset
                     offset = next_offset
@@ -207,7 +199,9 @@ class History(_MonitorThread):
                         account.offset = offset
                     offset = next_offset
                 else:
-                    print(f"biz: {account_biz} 当前offset: {offset}文章都存在 next_offset: {next_offset}")
+                    print(
+                        f"biz: {account_biz} 当前offset: {offset}文章都存在 next_offset: {next_offset}"
+                    )
                     if one_add or account_offset == 0:
                         account.offset = offset
                         offset = next_offset
@@ -238,13 +232,16 @@ class History(_MonitorThread):
             print("开始同步；", account)
             self.update_account(account, status=2)
             self.account_run(account_id)
-            self.update_account(account, status=0, update=str(int(time.time())))
+            self.update_account(account,
+                                status=0,
+                                update=str(int(time.time())))
             print("数据已同步；", account)
             if self.check_account_status(account_id, 2):
                 self.update_account(account, status=1)
 
 
 class Article(_MonitorThread):
+
     @staticmethod
     def get_comment_id_from_html(res_html):
         return re.search(r"comment_id = .*?\"([\d]+)\"", res_html).group(1)
@@ -282,7 +279,8 @@ class Article(_MonitorThread):
         finally:
             db.session.add(article)
             db.session.commit()
-            print("COMMIT", article.id, article.article_done, article.article_title)
+            print("COMMIT", article.id, article.article_done,
+                  article.article_title)
 
     def start_run(self):
         for article in self.articles(article_done=False):
@@ -293,14 +291,16 @@ class Article(_MonitorThread):
 
 
 class Comment(_MonitorThread):
+
     def start_run(self):
         article_list = models.Article.query.filter_by(
-            article_done=True,
-        ).filter(
-            models.Article.article_comment_id != 0,
-            models.Article.comment_update < int(time.time()) - WX_UPDATE_TIME,
-            models.Article.article_publish_time > models.Article.comment_update - WX_NOT_UPDATE_TIME,
-        ).all()
+            article_done=True, ).filter(
+                models.Article.article_comment_id != 0,
+                models.Article.comment_update <
+                int(time.time()) - WX_UPDATE_TIME,
+                models.Article.article_publish_time >
+                models.Article.comment_update - WX_NOT_UPDATE_TIME,
+            ).all()
 
         db.session.commit()
         print("Comment len(article_list): ", len(article_list))
@@ -313,7 +313,8 @@ class Comment(_MonitorThread):
     @staticmethod
     def save_comment(article_id, comment_dict):
         for comment_item in comment_dict['comments']:
-            if models.Comment.query.filter_by(content_id=str(comment_item["content_id"])).count() == 0:
+            if models.Comment.query.filter_by(
+                    content_id=str(comment_item["content_id"])).count() == 0:
                 comment = models.Comment(
                     user_name=comment_item["user_name"],
                     user_logo=comment_item["user_logo"],
@@ -321,17 +322,15 @@ class Comment(_MonitorThread):
                     datetime=str(comment_item["datetime"]),
                     content_id=str(comment_item["content_id"]),
                     like_count=int(comment_item["like_count"]),
-                    article_id=article_id
-                )
+                    article_id=article_id)
                 db.session.add(comment)
                 db.session.commit()
-            comment = models.Comment.query.filter_by(content_id="%s" % comment_item["content_id"]).first()
+            comment = models.Comment.query.filter_by(
+                content_id="%s" % comment_item["content_id"]).first()
             reply_list = comment_item["reply_list"]
             for reply_item in reply_list:
-                reply = models.CommentReply(
-                    **reply_item,
-                    comment_id=comment.id
-                )
+                reply = models.CommentReply(**reply_item,
+                                            comment_id=comment.id)
                 db.session.add(reply)
                 db.session.commit()
 
@@ -362,15 +361,17 @@ class Comment(_MonitorThread):
 
 
 class ReadLike(_MonitorThread):
+
     def start_run(self):
         article_list = models.Article.query.filter_by(
-            article_done=True,
-        ).filter(
-            models.Article.article_fail == False,
-            # update时间超过1天，那就要重新获取阅读量
-            models.Article.read_like_update < int(time.time()) - WX_UPDATE_TIME,
-            models.Article.article_publish_time > models.Article.read_like_update - WX_NOT_UPDATE_TIME,
-        ).all()
+            article_done=True, ).filter(
+                models.Article.article_fail == False,
+                # update时间超过1天，那就要重新获取阅读量
+                models.Article.read_like_update <
+                int(time.time()) - WX_UPDATE_TIME,
+                models.Article.article_publish_time >
+                models.Article.read_like_update - WX_NOT_UPDATE_TIME,
+            ).all()
         db.session.commit()
         print("ReadLike len(article_list): ", len(article_list))
         for article in article_list:
@@ -405,25 +406,8 @@ class ReadLike(_MonitorThread):
             check_key_uin(account_biz)
 
 
-@deprecated
-class KeyUin(_MonitorThread):
-    def start_run(self):
-        for account in self.accounts():
-            account_biz = account.account_biz
-            account_url = account.account_url
-            print(account, account_biz, account_url)
-            key_uin = _get_key_uin(account_biz)
-            if not key_uin:
-                print("got no key_uin")
-                get_pass_key_and_uin(account_url, account_biz)
-            else:
-                print("got key_uin:", key_uin)
-
-            time.sleep(1)
-
-
 if __name__ == '__main__':
-    #class_names = ["History", "Article", "Comment", "ReadLike", "KeyUin"]
+    #class_names = ["History", "Article", "Comment", "ReadLike"]
     # class_names = ["History"]
     # class_names = ["Article", "ReadLike"]
     class_names = sys.argv[1:]
